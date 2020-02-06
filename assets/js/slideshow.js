7e85831ab5e4ad204430cf0ffc6200e6ed41744d
Dbyu85 <50050452+Dbyu85@users.noreply.github.com>
2020-02-06 11:44:26 +0100

    Slideshow functions.

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,24 +1,26 @@
-var slide = 0;
+var slide = 1;
 
 showSlide(slide);
 
 //next/previous control
-function plusSlide (s) {
+function plusSlide(s) {
 	showSlide(slide += s);
 }
 
 
 function showSlide() {
 	var i;
-	
+
 	const slides = document.getElementsByClassName('slideShow');
-	
+
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = 'none';
 	}
 	slide++;
-	if (slide > slides.length) {slide = 1}
-	slides[slide-1].style.display = 'block';
-	setTimeout(showSlide, 5000);
-}
+	if (slide > slides.length) {
+		slide = 1
+	}
+	slides[slide - 1].style.display = 'block';
+	setTimeout(showSlide, 2000);
 
+}

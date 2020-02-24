a15d7abb964e19c5a6142591c79d82aa28a0e851
Dbyu85 <50050452+Dbyu85@users.noreply.github.com>
2020-02-24 20:43:32 +0100

    update.

---
+++ assets/js/presentation.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+	//hide answers for the faq section.
+	$(".answer").hide();
+
+	//showing answer with slideToggle method.
+	$(".faq h4").click(function () {
+		$(this).next(".answer").slideToggle();
+	});
+	$(".faq h4").addClass("faq-question");
+	//------------------end-faq
+	
+	//next and prev button for the slideshow
+	
+	$(".next").click (function(){
+		var currentImg = $(".active");
+		var nextImg = currentImg.next();
+		
+		if(nextImg.length){
+			currentImg.removeClass("active").css("z-index", -10);
+			nextImg.addClass("active").css("z-index", 10);
+		}
+	});
+	
+	$(".prev").click (function(){
+		var currentImg = $(".active");
+		var prevImg = currentImg.prev();
+		
+		if(prevImg.length){
+			currentImg.removeClass("active").css("z-index", -10);
+			prevImg.addClass("active").css("z-index", 10);
+		}
+	});
+	
+});
+
+function initMap() {
+				var map = new google.maps.Map(document.getElementById('map-locations'), {
+					zoom: 6,
+					center: {
+						lat: 20.593684,
+						lng: 78.96288
+					}
+				});
+
+				var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+				var locations = [{
+						lat: 15.335013,
+						lng: 76.460024
+					},
+					{
+						lat: 15.335015,
+						lng: 76.459998
+					},
+					{
+						lat: 15.34231,
+						lng: 76.475235
+					},
+					{
+						lat: 15.334745,
+						lng: 76.462146
+					},
+				];
+
+				var markers = locations.map(function(location, i) {
+					return new google.maps.Marker({
+						positon: location,
+						label: labels[i % labels.length]
+					});
+				});
+				var markerCluster = new MarkerClusterer(map, markers, {
+					imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+				});
+
+			}
+

009d4870ba1d6c866af2533e1f2d08f2fca8b9ea
Dbyu85 <lim658589@gmail.com>
2020-12-04 16:08:17 +0000

    Nothing to commit

--- assets/js/email.js
+++ assets/js/email.js
@@ -4,11 +4,5 @@ function sendMailContact(contacForm){
         "from_email": contactForm.emailaddress.value,
         "help_request": contactForm.projectsummary.value
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
+    
 }
